4a525517d7e4dada776d1a67dca6b3d8892010c1
Sebastien Bro <sebastienb@spotify.com>
2025-11-07 18:28:51 +0100

    Update service worker to auto-reload on new version
    - Force immediate reload when new content is available
    - Improves user experience by showing latest version without manual cache clear

--- src/serviceWorker.js
+++ src/serviceWorker.js
@@ -67,17 +67,18 @@ function registerValidSW(swUrl, config) {
           if (installingWorker.state === 'installed') {
             if (navigator.serviceWorker.controller) {
               // At this point, the updated precached content has been fetched,
-              // but the previous service worker will still serve the older
-              // content until all client tabs are closed.
+              // Force the new service worker to activate immediately
               console.log(
-                'New content is available and will be used when all ' +
-                  'tabs for this page are closed. See http://bit.ly/CRA-PWA.'
+                'New content is available! Refreshing to show the latest version.'
               );
 
               // Execute callback
               if (config && config.onUpdate) {
                 config.onUpdate(registration);
               }
+              
+              // Force reload to get the new version
+              window.location.reload();
             } else {
               // At this point, everything has been precached.
               // It's the perfect time to display a
